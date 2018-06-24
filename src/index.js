4542f19af90cdd38105b90bf594332623486e1c6
cutamar <amar.cutura@gmail.com>
2018-06-24 21:10:50 +0200

    fixed indent and added redux dev tools

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './styles/main.css';
 
 import HelloWorld from './components/hello_world';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// redux dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(
+    applyMiddleware()
+));
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/" component={HelloWorld} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  </Provider>
-  , document.getElementById('app'));
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route path="/" component={HelloWorld} />
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('app'));
