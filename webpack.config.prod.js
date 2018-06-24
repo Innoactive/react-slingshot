ab5744e46ac47d5b10e771cc7f311642d899649c
cutamar <amar.cutura@gmail.com>
2018-06-24 16:59:18 +0200

    prod config changed and added webpack-bundle-tracker

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -3,7 +3,7 @@
 import webpack from 'webpack';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import WebpackMd5Hash from 'webpack-md5-hash';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+import BundleTracker from 'webpack-bundle-tracker';
 import path from 'path';
 
 const GLOBALS = {
@@ -34,27 +34,8 @@ export default {
     // Generate an external css file with a hash in the filename
     new ExtractTextPlugin('[name].[md5:contenthash:hex:20].css'),
 
-    // Generate HTML file that contains references to generated bundles. See here for how this works: https://github.com/ampedandwired/html-webpack-plugin#basic-usage
-    new HtmlWebpackPlugin({
-      template: 'src/index.ejs',
-      favicon: 'src/favicon.ico',
-      minify: {
-        removeComments: true,
-        collapseWhitespace: true,
-        removeRedundantAttributes: true,
-        useShortDoctype: true,
-        removeEmptyAttributes: true,
-        removeStyleLinkTypeAttributes: true,
-        keepClosingSlash: true,
-        minifyJS: true,
-        minifyCSS: true,
-        minifyURLs: true
-      },
-      inject: true,
-      // Note that you can add custom options here if you need to handle other custom logic in index.html
-      // To track JavaScript errors via TrackJS, sign up for a free trial at TrackJS.com and enter your token below.
-      trackJSToken: ''
-    }),
+    // needed for django-webpack-loader
+    new BundleTracker({filename: './webpack-stats.json'}),
   
   ],
   module: {
